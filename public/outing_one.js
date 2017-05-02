7f677fd8b28a6fbd0bd88c9dbf76430983a4e91f
Guillaume Maurino <guillaume.maurino@gmail.com>
2017-05-01 22:55:55 -0700

    Working on the outing specific screen
    - Still todo: ability to update user. Need to think about datamodel
    - Still todo: ability to update / remove routes.
    - Should pull at least one image from routes API

--- public/outing_one.js
+++ public/outing_one.js
@@ -32,6 +32,60 @@ var input_participants = document.getElementById("aa-input-participants");
 //Handle add/removing a class based on if text has been entered in the search input
 //attach custom event handler - autocomplete:updated triggers when dataset is rendered
 
+function change_outing(outing_id) {
+  var change_outing_description = document.getElementById("change_outing_description");
+  var change_outing_conditions = document.getElementById("change_outing_conditions");
+
+  if(event.keyCode == 13) {
+    const ul = document.getElementById('outings_js');
+    const url = '/outings/' + outing_id;
+    console.log(url);
+      var request = new Request(url, {
+        method: 'GET',
+        headers: new Headers()
+      });
+
+      // Now use it!
+      fetch(request)
+      .then((resp) => resp.json())
+      .then(function(data) {
+            console.log('Front end - get outing:')
+            console.log(data);
+            // Here we should do a put to update the name !!!!
+            if (change_outing_description.value != ""){
+              data.description = change_outing_description.value;
+              console.log('Changing outing descriptions');
+            }
+            if (change_outing_conditions.value != ""){
+              data.conditions = change_outing_conditions.value;
+              console.log('Changing outing conditions');
+            }
+            var request = new Request(url, {
+              method: 'PUT',
+              body: JSON.stringify( data ),
+              headers: {
+                  'Accept': 'application/json, text/plain, */*',
+                  'Content-Type': 'application/json'
+              }
+            });
+            fetch(request)
+            .then((resp) => resp.json())
+            .then(function(data) {
+              console.log('Front end - put request result:')
+              console.log(data);
+              //location.reload();
+            })
+            .catch(function(error) {
+              console.log(error);
+            });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    }
+};
+
+
 function new_participants_fire(){
   // if the participants press enter;
   if(event.keyCode == 13) {
@@ -44,7 +98,6 @@ function post_new_participants(suggestion){
   // we are going to created a post
   var participants = {
     title: search_participants.value,
-    route_ids : []
   };
   if (suggestion != undefined){
     participants.title = suggestion.title_prefix + " - " + suggestion.title;
@@ -73,56 +126,3 @@ document.getElementById("icon-participants-close").addEventListener("click", fun
     search_participants.value = "";
     input_participants.classList.remove("input-has-value");
 });
-
-function change_participants(participants_id) {
-  var change_participants_description = document.getElementById("change_participants_description");
-  var change_participants_conditions = document.getElementById("change_participants_conditions");
-
-  if(event.keyCode == 13) {
-    const ul = document.getElementById('participantss_js');
-    const url = '/participantss/' + participants_id;
-    console.log(url);
-      var request = new Request(url, {
-        method: 'GET',
-        headers: new Headers()
-      });
-
-      // Now use it!
-      fetch(request)
-      .then((resp) => resp.json())
-      .then(function(data) {
-            console.log('Front end - get participants:')
-            console.log(data);
-            // Here we should do a put to update the name !!!!
-            if (change_participants_description.value != ""){
-              data.description = change_participants_description.value;
-              console.log('Changing participants descriptions');
-            }
-            if (change_participants_conditions.value != ""){
-              data.conditions = change_participants_conditions.value;
-              console.log('Changing participants conditions');
-            }
-            var request = new Request(url, {
-              method: 'PUT',
-              body: JSON.stringify( data ),
-              headers: {
-                  'Accept': 'application/json, text/plain, */*',
-                  'Content-Type': 'application/json'
-              }
-            });
-            fetch(request)
-            .then((resp) => resp.json())
-            .then(function(data) {
-              console.log('Front end - put request result:')
-              console.log(data);
-              location.reload();
-            })
-            .catch(function(error) {
-              console.log(error);
-            });
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-    }
-};
