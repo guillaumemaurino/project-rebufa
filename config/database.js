8e1b86fa0854267f9cf6a6dfcda97837836de23d
Guillaume Maurino <guillaume.maurino@gmail.com>
2017-05-10 15:15:44 -0700

    Adding mongo LAB to Heroku.

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 // config/database.js
 module.exports = {
-    'url' : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||'mongodb://localhost/ninjago' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.MONGODB_URI ||'mongodb://localhost/ninjago' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
   };
--- config/login.js
+++ config/login.js
@@ -6,7 +6,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'        : '1912762422329843', // your App ID
         'clientSecret'    : '1e1d5eb64f5daa85cbb2d0029ff79029', // your App Secret
-        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback',
+        'callbackURL'     : '/auth/facebook/callback',
         'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'displayName', 'email', 'picture.type(large)']
 
@@ -14,6 +14,6 @@ module.exports = {
     'googleAuth' : {
         'clientID'         : '1068653630104-qvob64k7i0vv0tlpstn8ssqe41dugd8k.apps.googleusercontent.com',
         'clientSecret'     : 'dbV9ArfF-4Vk2thAmbLz3z8a',
-        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'      : '/auth/google/callback'
     }
 };
