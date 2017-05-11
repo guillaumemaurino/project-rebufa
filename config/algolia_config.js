5c15158d28a87ee5097a4d8ba124bba7afad159c
guillaumemaurino <guillaume.maurino@gmail.com>
2017-05-11 11:27:36 -0700

    Updating config for rebufa sandbox

--- config/algolia_config.js
+++ config/algolia_config.js
@@ -1,9 +1,15 @@
 // config/database.js
 module.exports = {
+    
+    // Sand box rebufa
+    'applicationID' : 'S3FCFP2VKF',
+    'apiKey' : 'aa62956734b8254192bffde8804e5754',
+    'searchKey' : 'fc2f3b31e0ebad9ecc010f1481917e6a'     
+    
     // Guillauem algolia parameters....
-    'applicationID' : 'TR971CJDWI',
-    'apiKey' : '4e93ff805c19864e7e343011ef3dc93c',
-    'searchKey' : 'fc29e88ddda3305761a94ef15e700bd1'
+    //'applicationID' : 'TR971CJDWI',
+    //'apiKey' : '4e93ff805c19864e7e343011ef3dc93c',
+    //'searchKey' : 'fc29e88ddda3305761a94ef15e700bd1'
 
 
     // Antoine initialization parameters
