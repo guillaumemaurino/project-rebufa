6313cd18cfef13f6ad624d591a9b323018c68c37
guillaumemaurino <guillaume.maurino@gmail.com>
2017-05-11 11:30:36 -0700

    Algolia sandbox config

--- public/assets/public_conf.js
+++ public/assets/public_conf.js
@@ -1,7 +1,11 @@
 
+// Sandbox environment configuration...
+var applicationID = 'S3FCFP2VKF';
+var searchKey = 'fc2f3b31e0ebad9ecc010f1481917e6a';
+
 // Guillaume configuration...
-var applicationID = 'TR971CJDWI';
-var searchKey = 'fc29e88ddda3305761a94ef15e700bd1';
+// var applicationID = 'TR971CJDWI';
+// var searchKey = 'fc29e88ddda3305761a94ef15e700bd1';
 
 // Antoine configuration.....
 //var applicationID = 'S4KUBACX2X';
